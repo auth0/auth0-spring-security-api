96edccce81b94cac74f5c9005752cd1003a27c49
Anthony Drogon <anthony.drogon@gmail.com>
2016-10-13 23:54:57 +0100

    Replace deprecated FilterRegistrationBean

--- src/main/java/com/auth0/spring/security/api/Auth0SecurityConfig.java
+++ src/main/java/com/auth0/spring/security/api/Auth0SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
