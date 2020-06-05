fe1e06661cba554d0b871258d6345ce47af342cf
James Anderson <jim.anderson@auth0.com>
2020-06-05 16:06:00 -0500

    Add test for JwtAccessDeniedHandler

---
+++ lib/src/test/java/com/auth0/spring/security/api/JwtAccessDeniedHandlerTest.java
@@ -0,0 +1,28 @@
+package com.auth0.spring.security.api;
+
+import org.junit.Test;
+import org.springframework.security.access.AccessDeniedException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class JwtAccessDeniedHandlerTest {
+
+    @Test
+    public void shouldReturnForbidden() throws Exception {
+        JwtAccessDeniedHandler handler = new JwtAccessDeniedHandler();
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        AccessDeniedException exception = new AccessDeniedException("Forbidden");// mock(AccessDeniedException.class);
+
+        handler.handle(request, response, exception);
+        verify(response).addHeader(
+                "WWW-Authenticate",
+                "Bearer error=\"Insufficient scope\""
+        );
+        verify(response).sendError(403, "Forbidden");
+    }
+}
